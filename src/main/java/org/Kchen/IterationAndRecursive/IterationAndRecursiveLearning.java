de123b1e2efc4ee1d779cc53176376c9f0b87d88
kissinger <kchen0723@gmail.com>
2025-03-28 17:59:36 -0700

    add interationAndRecursiveLearning.java/org/Kchen/IterationAndRecursive/IterationAndRecursiveLearning.java

---
+++ src/main/java/org/Kchen/IterationAndRecursive/IterationAndRecursiveLearning.java
@@ -0,0 +1,9 @@
+package org.Kchen.IterationAndRecursive;
+
+public class IterationAndRecursiveLearning {
+    public static void IterationArray(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ src/test/java/org/Kchen/IterationAndRecursive/IterationAndRecursiveLearningTest.java
@@ -0,0 +1,13 @@
+package org.Kchen.IterationAndRecursive;
+
+import org.Kchen.IterationAndRecursive.*;
+import org.junit.jupiter.api.*;
+
+public class IterationAndRecursiveLearningTest {
+
+    @Test
+    public void TestIterationArray(){
+        int[] arr = new int[]{4, 7, 9, 11};
+        IterationAndRecursiveLearning.IterationArray(arr);
+    }
+}
