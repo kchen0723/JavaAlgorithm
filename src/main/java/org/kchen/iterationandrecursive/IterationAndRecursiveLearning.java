cb4d5019ac0f924b6e6a93968887031be2e40863
kissinger <kchen0723@gmail.com>
2025-04-02 21:26:28 -0700

    add new files

---
+++ src/main/java/org/kchen/iterationandrecursive/IterationAndRecursiveLearning.java
@@ -0,0 +1,9 @@
+package org.kchen.iterationandrecursive;
+
+public class IterationAndRecursiveLearning {
+    public static void IterationArray(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
